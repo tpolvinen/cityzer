1fd9f049cb199b21e14ede4b2e88334a811e7691
tatu-polvinen <tatu.polvinen@gmail.com>
2017-11-16 14:56:06 +0200

    Working release candidate for development

--- src/main/java/com/haagahelia/cityzer/CityzerApplication.java
+++ src/main/java/com/haagahelia/cityzer/CityzerApplication.java
@@ -2,26 +2,18 @@ package com.haagahelia.cityzer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class CityzerApplication extends SpringBootServletInitializer {
+public class CityzerApplication {
 
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(CityzerApplication.class);
-	}
-
-
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		SpringApplication.run(CityzerApplication.class, args);
 	}
 }
