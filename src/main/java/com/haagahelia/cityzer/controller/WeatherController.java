169461475ef125fa3089d7ffcebc732f54d28c9d
tatu-polvinen <tatu.polvinen@gmail.com>
2017-11-30 12:12:48 +0200

    Revert "Mock-up IF to WeatherController"
    This reverts commit c6bd89099bf7ac26826e001d3c1a4865a4bc34a3.

--- src/main/java/com/haagahelia/cityzer/controller/WeatherController.java
+++ src/main/java/com/haagahelia/cityzer/controller/WeatherController.java
@@ -1,7 +1,5 @@
 package com.haagahelia.cityzer.controller;
 
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,8 +9,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 import ucar.ma2.InvalidRangeException;
 
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.Date;
 
@@ -26,9 +22,6 @@ public class WeatherController {
     @Value("${json.filepath}")
     private String filepath;
 
-    @Value("${mockUpWeather.filepath}")
-    private String mockUpWeatherFilepath;
-
     @CrossOrigin
     @RequestMapping(value = "/api/getWeather", method = RequestMethod.GET, produces = "application/json")
     public JSONObject getWeather(@RequestParam double userLat, @RequestParam double userLon) throws IOException, InvalidRangeException {
@@ -39,23 +32,6 @@ public class WeatherController {
         JSONObject jsonObject;
 
         Date date = new Date();
-
-        JSONParser parser = new JSONParser();
-
-        // 60.2015 - 60.2025  24.9335 - 24.9345
-
-        if (userLat >= 60.2015 && userLat <= 60.2025 && userLon >= 24.9335 && userLon <= 24.9345) {
-
-            Object object = null;
-            try {
-                object = parser.parse(new FileReader(mockUpWeatherFilepath));
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-
-            jsonObject = (JSONObject) object;
-            return jsonObject;
-        }
 
         return JSON_Reader.weatherReader(userLat, userLon, filepath, date);
 
